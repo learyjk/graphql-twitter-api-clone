b2b8eb0bc94e0eb61ab2fea559558dd2edf55264
Keegan Leary <leary.keegan@gmail.com>
2022-07-23 10:21:42 -0700

    apollo plugin

--- server/src/utils/createServer.ts
+++ server/src/utils/createServer.ts
@@ -1,10 +1,25 @@
 import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
 import { ApolloServer } from "apollo-server-fastify";
-import fastify from "fastify";
+import { ApolloServerPlugin } from "apollo-server-plugin-base";
+import fastify, { FastifyInstance } from "fastify";
 import { buildSchema } from "type-graphql";
 import UserResolver from "../modules/user/user.resolver";
 
 const app = fastify();
+
+function fastifyAppClosePlugin(app: FastifyInstance): ApolloServerPlugin {
+  return {
+    async serverWillStart() {
+      console.log("SERVER WILL START");
+      return {
+        async drainServer() {
+          console.log("DRAIN SERVER");
+          await app.close();
+        },
+      };
+    },
+  };
+}
 
 function buildContext() {}
 
@@ -15,7 +30,10 @@ export async function createServer() {
 
   const server = new ApolloServer({
     schema,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer: app.server })],
+    plugins: [
+      fastifyAppClosePlugin(app),
+      ApolloServerPluginDrainHttpServer({ httpServer: app.server }),
+    ],
     context: buildContext,
   });
 
